2ca41a832324188e1124be1879dc1890c1400ff3
Nicholas Thomson <nthomsondev@gmail.com>
2016-01-28 10:39:29 -0500

    i suck

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name epiloggedApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the epiloggedApp
+ */
+angular.module('epiloggedApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
